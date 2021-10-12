cb4d74047e4498a81ae2c2e6ebd8ed1adb9508e1
Sven Reiche <sven.reiche@psi.ch>
2021-10-12 13:02:23 +0200

    Initial test with particle class/struct

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#include "src/Core/Particle.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Particle<double> p;
+    p.gamma=2;
+    Particle<double> pp = p;
+    std::cout << pp.gamma << " " << pp.theta << std::endl;
     return 0;
 }
--- src/Core/Particle.h
+++ src/Core/Particle.h
@@ -5,4 +5,13 @@
 #ifndef GENESIS_1_3_VERSION5_PARTICLE_H
 #define GENESIS_1_3_VERSION5_PARTICLE_H
 
+template<typename T>   // template to optimize memory consumption (either float or double)
+struct Particle{
+    T gamma = 1;        // energy in units of mc^2
+    T theta = 0;        // longitudinal position in units in units of 2 pi /lambda_ref
+    T x = 0;            // transverse position in x in units of m
+    T y = 0;            // transverse position in y in units of m
+    T px = 0;           // transverse momentum in x in units of mc
+    T py= 0;           // transverse momentum in y in units of mc
+};
 #endif //GENESIS_1_3_VERSION5_PARTICLE_H
